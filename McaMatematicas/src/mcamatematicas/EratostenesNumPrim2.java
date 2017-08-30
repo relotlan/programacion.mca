facdcfae7f78d411cb5cd8b2dc565b0ffe52ffb7
Elotlan <elotlan.rebeca@gmail.com>
2017-08-30 10:57:47 -0500

    Tarea de torres de Hanoi, con una formula que se calcula directamente los movimientos.

---
+++ McaMatematicas/src/mcamatematicas/EratostenesNumPrim2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcamatematicas;
+
+/**
+ *
+ * @author Lenovo Unee G40
+ */
+import java.util.*;
+
+public class EratostenesNumPrim2 {
+
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int mov = 0;
+
+        System.out.println("Introduce el número de discos a acomodar"); 
+        int discos = ob.nextInt();
+
+        mov = (int) (Math.pow(2, discos)) - 1;
+        System.out.println(mov);
+    }
+}
