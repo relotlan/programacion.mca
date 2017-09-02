1f76413915e3783d2c240e6c63b4d76834324859
Elotlan <elotlan.rebeca@gmail.com>
2017-09-02 14:30:28 -0500

    Ejercicio en donde saca el número mayor de la función

---
+++ JavaApplication7/src/McaProgramacion/Ej3_Mayor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package McaProgramacion;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo Unete G40
+ */
+public class Ej3_Mayor {
+
+    public static double f() {
+        double res, acom = 0;
+        for (double i = -5; i <= 5; i += 0.1) {
+            res = (Math.exp((-0.5 * i * i)) / (Math.sqrt(2 * Math.PI))) * 1000;
+            if (acom < res) {
+                acom = res;                
+            }
+        }
+        System.out.println(acom);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        f();
+
+    }
+
+}
