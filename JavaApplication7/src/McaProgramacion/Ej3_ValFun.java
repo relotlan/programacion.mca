b7a770cc7825e8a43ec821c8d0a9dd8d6542432e
Elotlan <elotlan.rebeca@gmail.com>
2017-09-02 14:25:36 -0500

    Ejercicio de graficación

--- JavaApplication7/src/McaProgramacion/Ej3_ValFun.java
+++ JavaApplication7/src/McaProgramacion/Ej3_ValFun.java
@@ -16,31 +16,38 @@ public class Ej3_ValFun {
     public static double f() {
         double res;
         int ent, k, col;
+        ArrayList<Integer> acom = new ArrayList();
         String graf[][] = new String[400][101];
-        
+
         for (int j = 0; j < 400; j++) {//Filas
             //System.out.println("---------------------"+j+"--------------------------");
-            col=0;
+            col = 0;
             for (double i = -5; i <= 5; i += 0.1) {//columnas                
                 k = (int) (50 + (i * 10)); //Se pasan a enteros i para acomodar las columnas
                 res = (Math.exp((-0.5 * i * i)) / (Math.sqrt(2 * Math.PI))) * 1000; //Generá la función
                 ent = (int) res; //Se toman los enteros del resultado de la función para acomodar las filas
 //                System.out.println(ent+"  "+j+" "+col);
                 if (j == ent) {
-                    
-                   graf[j][col] = "*";
-               } else {
-                   graf[j][col] = " ";
+
+                    graf[j][col] = "*";
+                } else {
+                    graf[j][col] = " ";
                 }
                 col++;
             }
-            
+
         }
 
-       for (int m = 0; m < 400; m++) {
+        for (double i = -5; i <= 5; i += 0.1) {//columnas  
+            res = (Math.exp((-0.5 * i * i)) / (Math.sqrt(2 * Math.PI))) * 1000;
+            acom.add((int) res);
+        }
+
+        for (int m = 0; m < 400; m++) {
             for (int n = 0; n < 101; n++) {
-                    System.out.print(graf[m][n]);
-                } 
+                System.out.print(graf[m][n]);
+
+            }
             System.out.println("");
         }
 
